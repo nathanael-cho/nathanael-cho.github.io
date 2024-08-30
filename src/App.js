e531c46c91da81e7000312fd7c32d9bc22c18054
Nathanael Cho <nathanael.a.cho@gmail.com>
2024-08-30 16:06:00 -0400

    Clean up files of fluff I want control over, and add Mantine for styling/components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import '@mantine/core/styles.css';
+
 function App() {
   return (
     <div>This is my personal blog!</div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.css';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MantineProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
