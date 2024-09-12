d97735654f36641e96328bc6d27ce147c1ec6462
Nathanael Cho <ncho@harbourvest.com>
2024-09-12 15:47:51 -0400

    Basic skeleton of the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 import '@mantine/core/styles.css';
 
 function App() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <div>This is my personal blog!</div>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'md',
+        collapsed: { mobile: !opened, desktop: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          size="md"
+        />
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">
+        Post 1
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        This is my blog!
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
