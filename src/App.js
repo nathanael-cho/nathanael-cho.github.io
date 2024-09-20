86e16ae3fd0e144f797683f62f5b5099da9c9ae0
Nathanael Cho <ncho@harbourvest.com>
2024-09-20 15:25:39 -0400

    Work on page on 9/20 - add painting to home page, and add first post!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import './App.css';
-import { AppShell, Burger } from '@mantine/core';
+import { AppShell, Burger, Center, Image, NavLink, Space, Text, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { IconBook, IconHome } from '@tabler/icons-react';
 
 import '@mantine/core/styles.css';
+
+function HomeContent() {
+  return (
+    <div>
+      <Center>
+        <Image
+          radius="md"
+          src='/dalle-home-page-painting.webp'
+          style={{width: 500}}
+        />
+      </Center>
+      <Space h={10}/>
+      <Center>Welcome to my personal blog!</Center>
+      </div>
+  )
+}
+
+function FirstPost() {
+  return (
+    <div>
+      <Title>Why is my nickname Nacho?</Title>
+      <Space h="md"/>
+      <Text>
+        My full name is Nathanael Andrew Cho.
+        My first initial is N, and my middle initial is A. Together
+        with my last name, you get Nacho!
+      </Text>
+    </div>
+  )
+}
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
@@ -17,20 +49,34 @@ function App() {
       }}
       padding="md"
     >
-      <AppShell.Header>
+      <AppShell.Header p="md" style={{background: '#aed6f1'}}>
         <Burger
           opened={opened}
           onClick={toggle}
-          size="md"
+          size="sm"
         />
       </AppShell.Header>
 
       <AppShell.Navbar p="md">
-        Post 1
+        <NavLink
+          href="/"
+          label="Home"
+          leftSection={<IconHome/>}
+        />
+        <NavLink
+          href="/post-1"
+          label="Why is my nickname Nacho?"
+          leftSection={<IconBook/>}
+        />
       </AppShell.Navbar>
 
       <AppShell.Main>
-        This is my blog!
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomeContent/>}/>
+            <Route path="/post-1" element={<FirstPost/>}/>
+          </Routes>
+        </BrowserRouter>
       </AppShell.Main>
     </AppShell>
   );
