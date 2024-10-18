b4d90771035b53107494520ecc739d6c99c338d2
Nathanael Cho <ncho@harbourvest.com>
2024-10-18 15:19:53 -0400

    Second post done!

--- src/pages/posts/SecondPost.js
+++ src/pages/posts/SecondPost.js
@@ -50,9 +50,60 @@ function SecondPost() {
             <p>
                 Let <Latex>$a = (x_a, y_a), b = (x_b, y_b), x_3 = (x_c, y_c)$</Latex> be
                 the three points. To start, we choose to move <Latex>$a$</Latex>.
+                Cue the hand wave! We immediately notice that there are only two
+                places <Latex>$a$</Latex> can move to such that the triangle is equilateral.
             </p>
 
-            <p>TODO.</p>
+            <p>
+                What is a closed form for the coordinates of those two points?
+                We call upon an old friend, the two-dimensional rotation
+                matrix:
+            </p>
+            <p>
+                <Latex displayMode>{
+                    '$\\begin{pmatrix} \\cos\\theta & -\\sin\\theta \\\\'
+                    + '\\sin\\theta & \\cos\\theta \\end{pmatrix}$'
+                }</Latex>
+            </p>
+                
+            <p>
+                Let <Latex>{'$\\vec{a}$, $\\vec{b}$, $\\vec{c}$'}</Latex> be
+                the vector forms of the three points. Then, we have the following closed form
+                for where <Latex>{'$a$'}</Latex> can move to:
+            </p>
+
+            <p>
+                <Latex displayMode>{
+                    '$$\\vec{c} +'
+                    + '\\begin{pmatrix} \\cos\\pm\\frac{\\pi}{3} & -\\sin\\pm\\frac{\\pi}{3} \\\\'
+                    + '\\sin\\pm\\frac{\\pi}{3} & \\cos\\pm\\frac{\\pi}{3} \\end{pmatrix}'
+                    + '(\\vec{b} - \\vec{c})$$'
+                }</Latex>
+            </p>
+
+            <p>
+                Said in words, we get the two possible locations for <Latex>{'$a$'}</Latex> by
+                rotating <Latex>{'$b$'}</Latex> around <Latex>{'$c$'}</Latex> by <Latex>{'$\\frac{\\pi}{3}$'}</Latex> radians in both directions.
+            </p>
+
+            <p>
+                To close out this first part, the way to get the minimum distance is as follows:
+            </p>
+
+            <ol>
+                <li>
+                    For each of <Latex>{'$a, b, c$'}</Latex>, calculate the two
+                    possible destinations.
+                </li>
+                <li>
+                    For each of the two destinations, calculate the distance
+                    to the original location.
+                </li>
+                <li>
+                    Pick the original point/destination point pair
+                    that minimizes the travel distance.
+                </li>
+            </ol>
         </div>
     )
     
